5da89ae3590070080c1de7c2cb28b4741dd8cfe3
Harish Charan <charan.harish@gmail.com>
2024-11-21 12:43:39 +0000

    Added a file multply.java to get product of two mumbers

---
+++ multiply.java
@@ -0,0 +1,10 @@
+public class MultiplyTwoNumbers {
+    public static void main(String[] args) {
+        double num1 = 10; // First number
+        double num2 = 20; // Second number
+        double mul = num1 * num2; // Calculate the sum
+
+        System.out.println("The product of " + num1 + " and " + num2 + " is: " + mul);
+    }
+}
+
