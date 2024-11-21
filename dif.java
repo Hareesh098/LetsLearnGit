8eaa9c6570b0abfba4e3219161e97b415703fff8
Harish Charan <charan.harish@gmail.com>
2024-11-21 12:25:03 +0000

    Added dif.java and changed the variable type in sum.java from int to double

---
+++ dif.java
@@ -0,0 +1,11 @@
+public class DifTwoNumbers {
+    public static void main(String[] args) {
+        double num1 = 10; // First number
+        double num2 = 20; // Second number
+        double dif = num1 - num2; // Calculate the sum
+
+        System.out.println("The difference of " + num1 + " and " + num2 + " is: " + dif);
+    }
+}
+
+
